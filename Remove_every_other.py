e9ec298ed3f58f0dcba918eaa74f6fae0c830366
Rohith04MVK <007rohitjj04@gmail.com>
2020-10-26 13:06:39 +0530

    initial commit

--- Remove_every_other.py
+++ Remove_every_other.py
@@ -4,7 +4,9 @@ remove_every_other([5,1,2,4,1]) # [5,2,1]
 remove_every_other([1]) # [1] 
 '''
 
-my_list = [1,2,3,4,5,6,7,8,9]
+
+
+my_list = [1,2,3,4,5]
 
 def remove_every_other(list1):
     n = 1
